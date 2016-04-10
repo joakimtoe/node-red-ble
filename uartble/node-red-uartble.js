e5894a8089742f2718e32b752ad2263cec33c272
Joakim Andre Tønnesen <joakimtoe@gmail.com>
2016-04-11 00:30:58 +0200

    First version of uartBLE node, only RX

---
+++ uartble/node-red-uartble.js
@@ -0,0 +1,57 @@
+module.exports = function(RED) {
+    "use strict";
+    var UartBLE = require("uartble");
+
+    function UartBLENode(n) {
+        RED.nodes.createNode(this,n);
+        this.name = n.name;
+        this.topic = n.topic;
+        this.uuid = n.uuid;
+        this.keys = n.keys;
+        if (this.uuid === "") { this.uuid = undefined; }
+        var node = this;
+        node.discovering = false;
+
+        node.loop = setInterval(function() {
+            if (!node.discovering) {
+                node.status({fill:"blue", shape:"dot", text:"discovering..."});
+                node.discovering = true;
+                UartBLE.discover(function(UartBLE) {
+                    node.status({fill:"blue", shape:"dot", text:"connecting"});
+                    //node.stag = UartBLE;
+                    node.log("found ble_app_uart: " + UartBLE._peripheral.uuid);
+                    node.topic = node.topic || UartBLE._peripheral.uuid;
+
+                    UartBLE.on('disconnect', function() {
+                        node.discovering = false;
+                        node.status({fill:"red", shape:"ring", text:"disconnected"});
+                        node.log("disconnected ",node.uuid);
+                    });
+
+                    UartBLE.connectAndSetUp(function(error) {
+                        node.log("connected to ble_app_uart: " + UartBLE._peripheral.uuid);
+                        node.status({fill:"green", shape:"dot", text:"connected"});
+
+                        UartBLE.on('dataRecvd', function(data){
+                            node.log('dataRecvd: ' + data);
+                            var data_str = data.toString('utf8')
+                            var msg = { payload:data_str }
+                            node.send(msg);
+                        });
+
+                        UartBLE.start(function(error, data) {
+                          console.log('Started: ' + data);
+                        });
+                    });
+                });
+            }
+        },15000);
+
+        this.on("close", function() {
+            if (node.loop) { clearInterval(node.loop); }
+            //if (node.stag) { node.stag.disconnect(function() {}); }
+        });
+    }
+
+    RED.nodes.registerType("uartBLE", UartBLENode);
+}
