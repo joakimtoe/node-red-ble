415d9ead6667e5c92c79c37e268561e993027fdf
Joakim Andre Tønnesen <joakimtoe@gmail.com>
2016-04-12 00:09:37 +0200

    Fixed missing reconnect issue when deploying flow and added support for TX

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-red-ble
 Node-red nodes for talking BLE with Nordic Semiconductors ble_app_uart SDK example and other BLE peripherals.
+# uartBLE
+Supports ble_app_uart SDK examples running on Nordic's development kits: 
+http://developer.nordicsemi.com/
+http://infocenter.nordicsemi.com/
--- uartble/node-red-uartble.js
+++ uartble/node-red-uartble.js
@@ -6,52 +6,61 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         this.name = n.name;
         this.topic = n.topic;
-        this.uuid = n.uuid;
-        this.keys = n.keys;
-        if (this.uuid === "") { this.uuid = undefined; }
+        this.ble_addr = n.ble_addr;
+        if (this.ble_addr === "") { this.ble_addr = undefined; }
         var node = this;
         node.discovering = false;
 
         node.loop = setInterval(function() {
-            if (!node.discovering) {
-                node.status({fill:"blue", shape:"dot", text:"discovering..."});
-                node.discovering = true;
-                UartBLE.discover(function(UartBLE) {
-                    node.status({fill:"blue", shape:"dot", text:"connecting"});
-                    //node.stag = UartBLE;
-                    node.log("found ble_app_uart: " + UartBLE._peripheral.uuid);
-                    node.topic = node.topic || UartBLE._peripheral.uuid;
+          if (!node.discovering) {
+              node.status({fill:"blue", shape:"dot", text:"discovering..."});
+              node.discovering = true;
+              UartBLE.discover(function(UartBLE) {
+                  node.status({fill:"blue", shape:"dot", text:"connecting"});
+                  //node.UartBLE = UartBLE;
+                  node.log("found ble_app_uart: " + UartBLE._peripheral.uuid);
+                  node.topic = node.topic || UartBLE._peripheral.uuid;
 
-                    UartBLE.on('disconnect', function() {
-                        node.discovering = false;
-                        node.status({fill:"red", shape:"ring", text:"disconnected"});
-                        node.log("disconnected ",node.uuid);
-                    });
+                  UartBLE.on('disconnect', function() {
+                      node.discovering = false;
+                      node.status({fill:"red", shape:"ring", text:"disconnected"});
+                      node.log("disconnected ",node.uuid);
+                  });
 
-                    UartBLE.connectAndSetUp(function(error) {
-                        node.log("connected to ble_app_uart: " + UartBLE._peripheral.uuid);
-                        node.status({fill:"green", shape:"dot", text:"connected"});
+                  UartBLE.connectAndSetUp(function(error) {
+                      node.log("connected to ble_app_uart: " + UartBLE._peripheral.uuid);
+                      node.status({fill:"green", shape:"dot", text:"connected"});
 
-                        UartBLE.on('dataRecvd', function(data){
-                            node.log('dataRecvd: ' + data);
-                            var data_str = data.toString('utf8')
-                            var msg = { payload:data_str }
-                            node.send(msg);
-                        });
+                      node.UartBLE = UartBLE;
 
-                        UartBLE.start(function(error, data) {
-                          console.log('Started: ' + data);
-                        });
-                    });
-                });
-            }
+                      UartBLE.on('dataRecvd', function(data){
+                          node.log('dataRecvd: ' + data);
+                          var data_str = data.toString('utf8')
+                          var msg = { payload:data_str }
+                          node.send(msg);
+                      });
+
+                      UartBLE.start(function(error, data) {
+                        node.log('Started: ' + data);
+                      });
+                  });
+              });
+          }
         },15000);
 
-        this.on("close", function() {
-            if (node.loop) { clearInterval(node.loop); }
-            //if (node.stag) { node.stag.disconnect(function() {}); }
+        node.on('input', function(msg) {
+            //new Buffer('Hei på deg verden!')
+            if (node.UartBLE) {
+                node.UartBLE.send(Buffer(msg.payload), function(){
+                    node.log('Sent!');
+                });
+            }
+        });
+
+        node.on("close", function() {
+          if (node.loop) { clearInterval(node.loop); }
+          if (node.UartBLE) { node.UartBLE.disconnect(function() {node.log('Disconnect');}); }
         });
     }
-
     RED.nodes.registerType("uartBLE", UartBLENode);
 }
